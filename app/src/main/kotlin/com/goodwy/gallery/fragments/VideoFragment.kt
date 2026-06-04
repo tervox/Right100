d9dd7da2701a0e4ea161d4bfe4c511f60695ca45
tervox <marcosnozz@hotmail.com>
2026-06-04 20:03:07 -0300

    Fix: Adicionar override no handleEvent e corrigir toast na linha 439

--- app/src/main/kotlin/com/goodwy/gallery/fragments/VideoFragment.kt
+++ app/src/main/kotlin/com/goodwy/gallery/fragments/VideoFragment.kt
@@ -969,7 +969,7 @@ class VideoFragment : ViewPagerFragment(), TextureView.SurfaceTextureListener,
 
     private fun doSkip(forward: Boolean) = skip(forward)
 
-    fun handleEvent(event: MotionEvent) {
+    override fun handleEvent(event: MotionEvent) {
         // Implementação simplificada de gestos se necessário
     }
 
