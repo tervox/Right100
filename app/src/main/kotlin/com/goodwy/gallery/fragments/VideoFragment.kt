23e80b9dcdbce30629237882dbb2adf0f0aa25af
tervox <marcosnozz@hotmail.com>
2026-06-04 19:25:55 -0300

    Fix: Remover override do handleEvent (classe final) e corrigir toast linha 439

--- app/src/main/kotlin/com/goodwy/gallery/fragments/VideoFragment.kt
+++ app/src/main/kotlin/com/goodwy/gallery/fragments/VideoFragment.kt
@@ -436,7 +436,7 @@ class VideoFragment : ViewPagerFragment(), TextureView.SurfaceTextureListener,
     private fun updatePlayerMuteState(showToast: Boolean = false) {
         if (!mHasAudio) {
             if (showToast && mWasVideoStarted) {
-                toast(R.string.video_no_sound)
+                Toast.makeText(requireContext(), "Pause o video primeiro", Toast.LENGTH_SHORT).show()
             }
         }
 
@@ -969,7 +969,7 @@ class VideoFragment : ViewPagerFragment(), TextureView.SurfaceTextureListener,
 
     private fun doSkip(forward: Boolean) = skip(forward)
 
-    override fun handleEvent(event: MotionEvent) {
+    fun handleEvent(event: MotionEvent) {
         // Implementação simplificada de gestos se necessário
     }
 
