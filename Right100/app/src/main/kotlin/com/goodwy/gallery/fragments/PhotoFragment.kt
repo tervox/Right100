4357876339abc867712f0fee16d40c7398b51f52
tervox <160701592+tervox@users.noreply.github.com>
2026-06-07 18:09:02 -0300

    Fix: Correct SubsamplingScaleImageView properties and clean up imports

--- Right100/app/src/main/kotlin/com/goodwy/gallery/fragments/PhotoFragment.kt
+++ Right100/app/src/main/kotlin/com/goodwy/gallery/fragments/PhotoFragment.kt
@@ -1,4 +1,5 @@
 package com.goodwy.gallery.fragments
+import com.davemorrissey.labs.subscaleview.SubsamplingScaleImageView
 
 import android.annotation.SuppressLint
 import android.content.res.Configuration
@@ -28,25 +29,21 @@ import androidx.core.view.updateLayoutParams
 import androidx.exifinterface.media.ExifInterface.*
 import com.alexvasilkov.gestures.GestureController
 import com.alexvasilkov.gestures.State
-import com.davemorrissey.labs.subscaleview.SubsamplingScaleImageView
-
 import com.bumptech.glide.Glide
 import com.bumptech.glide.Priority
 import com.bumptech.glide.load.DataSource
 import com.bumptech.glide.load.DecodeFormat
+import com.bumptech.glide.load.MultiTransformation
 import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.load.resource.bitmap.CenterCrop
 import com.bumptech.glide.load.resource.bitmap.Rotate
 import com.bumptech.glide.request.RequestListener
 import com.bumptech.glide.request.RequestOptions
 import com.bumptech.glide.request.target.Target
-import jp.wasabeef.glide.transformations.BlurTransformation
-import com.bumptech.glide.load.MultiTransformation
-import com.bumptech.glide.load.resource.bitmap.CenterCrop
 import com.davemorrissey.labs.subscaleview.DecoderFactory
 import com.davemorrissey.labs.subscaleview.ImageDecoder
 import com.davemorrissey.labs.subscaleview.ImageRegionDecoder
-import com.davemorrissey.labs.subscaleview.SubsamplingScaleImageView
 import com.github.penfeizhou.animation.apng.APNGDrawable
 import com.github.penfeizhou.animation.avif.AVIFDrawable
 import com.github.penfeizhou.animation.webp.WebPDrawable
@@ -70,18 +67,21 @@ import com.goodwy.gallery.svg.SvgSoftwareLayerSetter
 import com.squareup.picasso.Callback
 import com.squareup.picasso.Picasso
 import it.sephiroth.android.library.exif2.ExifInterface
-import org.apache.sanselan.common.byteSources.ByteSourceInputStream
-import org.apache.sanselan.formats.jpeg.JpegImageParser
-import pl.droidsonroids.gif.InputSource
 import java.io.File
 import java.io.FileOutputStream
 import java.util.Locale
+import jp.wasabeef.glide.transformations.BlurTransformation
 import kotlin.math.abs
 import kotlin.math.ceil
+import org.apache.sanselan.common.byteSources.ByteSourceInputStream
+import org.apache.sanselan.formats.jpeg.JpegImageParser
+import pl.droidsonroids.gif.InputSource
+
+
+
 
 class PhotoFragment : ViewPagerFragment() {
     private val DEFAULT_DOUBLE_TAP_ZOOM = 2f
-fun SubsamplingScaleImageView.isZoomedOut() = scale <= minScale
     private val ZOOMABLE_VIEW_LOAD_DELAY = 100L
     private val SAME_ASPECT_RATIO_THRESHOLD = 0.01
 
@@ -1039,3 +1039,5 @@ fun SubsamplingScaleImageView.isZoomedOut() = scale <= minScale
         }
     }
 }
+
+fun SubsamplingScaleImageView.isZoomedOut() = scale <= 1.0f
