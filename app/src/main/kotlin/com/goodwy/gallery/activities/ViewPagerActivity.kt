417ddbc2dbf3ec6ef11d28596a4d21d2ca9b9bc8
tervox <160701592+tervox@users.noreply.github.com>
2026-06-07 18:00:44 -0300

    Fix: Sync correct files to root app folder for build. Resolves unresolved references.

--- app/src/main/kotlin/com/goodwy/gallery/activities/ViewPagerActivity.kt
+++ app/src/main/kotlin/com/goodwy/gallery/activities/ViewPagerActivity.kt
@@ -192,7 +192,7 @@ class ViewPagerActivity : BaseViewerActivity(), ViewPager.OnPageChangeListener, 
         finish()
     }
 
-    fun refreshMenuItems() {
+    override fun refreshMenuItems() {
         val currentMedium = getCurrentMedium() ?: return
         currentMedium.isFavorite = mFavoritePaths.contains(currentMedium.path)
         val visibleBottomActions = if (config.bottomActions) config.visibleBottomActions else 0
