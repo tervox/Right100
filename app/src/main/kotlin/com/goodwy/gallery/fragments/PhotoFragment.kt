42b36e7ffc8a7ec4d891bd627c552946911411a0
tervox <160701592+tervox@users.noreply.github.com>
2026-06-07 19:42:13 -0300

    Refine: Photo Blur (High Quality) and OCR preprocess

--- app/src/main/kotlin/com/goodwy/gallery/fragments/PhotoFragment.kt
+++ app/src/main/kotlin/com/goodwy/gallery/fragments/PhotoFragment.kt
@@ -430,10 +430,10 @@ class PhotoFragment : ViewPagerFragment() {
         if (shouldBlur) {
             binding.photoBlurBg.beVisible()
             binding.photoBlurOverlay.beVisible()
-            binding.photoBlurOverlay.setBackgroundColor(0x15000000)
+            binding.photoBlurOverlay.setBackgroundColor(0x22000000)
             // MultiTransformation: CenterCrop preenche sem distorcer + BlurTransformation
             val options = RequestOptions()
-                .transform(MultiTransformation(CenterCrop(), BlurTransformation(25, 3)))
+                .transform(MultiTransformation(CenterCrop(), BlurTransformation(25, 4)))
             Glide.with(ctx)
                 .asBitmap()
                 .load(mMedium.path)
