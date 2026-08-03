8cb446e022359a6399b1ca81d754b508fdb7207f
tervox <marcosnozz@hotmail.com>
2026-08-02 23:54:54 -0300

    Fix ViewPager fragment recreation on delete

--- app/src/main/kotlin/com/goodwy/gallery/activities/ViewPagerActivity.kt
+++ app/src/main/kotlin/com/goodwy/gallery/activities/ViewPagerActivity.kt
@@ -466,7 +466,6 @@ class ViewPagerActivity : BaseViewerActivity(), ViewPager.OnPageChangeListener, 
         mMediums.removeAt(mPos)
         if (mMediums.isEmpty()) { finish(); return }
         if (mPos >= mMediums.size) mPos = mMediums.size - 1
-        (binding.viewPager.adapter as? MyPagerAdapter)?.notifyDataSetChanged()
         binding.viewPager.setCurrentItem(mPos, false)
         updateTitle()
         refreshMenuItems()
